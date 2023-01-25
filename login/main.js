73243052f510e33d3d8e18f47dad7e0e58df5ffe
Davide <davide.n2@gmx.de>
2023-01-25 12:22:17 +0100

    Login und Drag & Drop

---
+++ login/main.js
@@ -0,0 +1,44 @@
+setURL("https://davide-niola.developerakademie.net/smallest_backend_ever");
+let users = [];
+
+async function init() {
+  await downloadFromServer();
+  let item = backend.getItem("users");
+  if (typeof item === "string") {
+    users = JSON.parse(item) || [];
+  } else {
+    users = item;
+  }
+  await backend.setItem("users", users);
+}
+
+
+async function addUser() {
+  let newEmail = document.getElementById("email");
+  let newPassword = document.getElementById("password");
+  users.push({ email: newEmail.value, password: newPassword.value });
+  await backend.setItem("users", users);
+  console.log(users);
+  newEmail.value = "";
+  newPassword.value = "";
+  setTimeout(() => {
+    window.location.href =
+      "login.html?msg=Du hast dich erfolgreich Regrestiert";
+  }, 3000);
+}
+
+
+function login() {
+  let email = document.getElementById("email");
+  let password = document.getElementById("password");
+  let user = users.find(
+    (u) => u.email == email.value && u.password == password.value
+  );
+  console.log(user);
+  if (user) {
+    console.log("user gefunden");
+    setTimeout(() => {
+      window.location.href = "dashboard.html";
+    }, 3000);
+  }
+}
