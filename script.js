e607a7b818f80090989fab055f3dcc1708d4ec7c
Davide <davide.n2@gmx.de>
2023-02-04 10:42:27 +0100

    D-18/AddTask Bugfix

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ setURL("https://gruppe-446.developerakademie.net/smallest_backend_ever");
 let users = [];
 let allTasks = [];
 let currentUser = [];
+let categorys = [];
 
 function init() {
     loadCurrentUser()
