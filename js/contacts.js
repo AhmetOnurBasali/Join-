eb92991f82a1f04538070be6f8c0bd35b992773c
Davide <davide.n2@gmx.de>
2023-02-12 19:07:51 +0100

    D-34/Contacts/Add Edit & data preparation

--- js/contacts.js
+++ js/contacts.js
@@ -1,11 +1,30 @@
-
-
-function closeAddNewContact(){
-    let container = document.getElementById('addContactPopup')
-    container.classList.add('d-none')
+function closeAddNewContact() {
+  let container = document.getElementById("addContactPopup");
+  container.classList.add("d-none");
 }
 
-function openAddNewContact(){
-    let container = document.getElementById('addContactPopup')
-    container.classList.remove('d-none')
+function openAddNewContact() {
+  let container = document.getElementById("addContactPopup");
+  container.classList.remove("d-none");
+}
+
+function closeEditContact() {
+  let container = document.getElementById("editContactPopup");
+  container.classList.add("d-none");
+}
+
+function createNewContact(event) {
+  event.preventDefault();
+  let nameInput = document.getElementById("inputName").value;
+  let emailInput = document.getElementById("inputEmail").value;
+  let numberInput = document.getElementById("inputNumber").value;
+  console.log(nameInput, emailInput, numberInput);
+  console.log(currentUser);
+  let currentUserContacts = {
+    contactCreatorID: currentUser['id'],
+    name:nameInput,
+    email:emailInput,
+    phone:numberInput
+  }
+  console.log(currentUserContacts);
 }
