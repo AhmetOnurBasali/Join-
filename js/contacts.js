83baae468686ddad901689c4e378b61975f1706a
Davide <davide.n2@gmx.de>
2023-02-16 17:03:23 +0100

    D-37/Contacts

--- js/contacts.js
+++ js/contacts.js
@@ -1,4 +1,30 @@
-let currentUserContacts = []
+let currentUserContacts = [];
+
+async function loadContactsData() {
+  await downloadFromServer();
+  let item = await backend.getItem(`userID${currentUser["id"]}Contacts`);
+  currentUserContacts = item || [];
+  await test();
+  render();
+}
+async function test() {
+  let newContact;
+  if (
+    currentUserContacts == null ||
+    currentUserContacts.length == 0 ||
+    currentUserContacts == undefined
+  ) {
+    newContact = {
+      contactCreatorID: currentUser["id"],
+      name: currentUser.name,
+      email: currentUser.email,
+      phone: "Edit your Number",
+      contactID: 0,
+    };
+    currentUserContacts.push(newContact);
+    await backend.setItem(`userID${currentUser["id"]}Contacts`, currentUserContacts);
+  }
+}
 
 function closeAddNewContact() {
   let container = document.getElementById("addContactPopup");
@@ -21,38 +47,46 @@ async function createNewContact(event) {
   let emailInput = document.getElementById("inputEmail").value;
   let numberInput = document.getElementById("inputNumber").value;
   let currentContactID = currentUserContacts.length;
-  let newContact = {
+  newContact = {
     contactCreatorID: currentUser["id"],
     name: nameInput,
     email: emailInput,
     phone: numberInput,
     contactID: currentContactID,
   };
-  
-  
-   currentUserContacts.push(newContact);
-
-   await backend.setItem("currentUserContacts", (currentUserContacts));
+  currentUserContacts.push(newContact);
+  await backend.setItem(
+    `userID${currentUser["id"]}Contacts`,
+    currentUserContacts
+  );
   closeAddNewContact();
-  render();
 }
 
-async function render() {
-  await downloadFromServer();
+
+
+function render() {
   let dropArea = document.getElementById("contactsArea");
   dropArea.innerHTML = "";
+
+  currentUserContacts.sort((a, b) => {
+    if (a.contactID === 0) return -1;
+    if (b.contactID === 0) return 1;
+    return a.name.localeCompare(b.name);
+  });
+
+  let currentLetter = "";
   for (let i = 0; i < currentUserContacts.length; i++) {
     const contact = currentUserContacts[i];
-    dropArea.innerHTML += `<div>${contact.name}</div>`;
+    let firstLetter = contact.name[0].toUpperCase();
+    if (firstLetter !== currentLetter) {
+      dropArea.innerHTML += `<h2>${firstLetter}</h2>`;
+      currentLetter = firstLetter;
+    }
+    dropArea.innerHTML += `
+      <div>${contact.name}</div>
+      <div>${contact.email}</div>
+      <div>${contact.phone}</div>
+      <div>contact id: ${contact.contactID}</div>
+    `;
   }
-}
-
-async function loadContactsData() {
-  let item = await backend.getItem("currentUserContacts");
-  if (item !== null) {
-    currentUserContacts = JSON.parse(item);
-  } else {
-    currentUserContacts = [];
-  }
-  render();
 }
