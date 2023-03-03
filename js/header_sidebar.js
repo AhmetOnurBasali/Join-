36c1574b1d2091a05bb72663bec3eca624b647d4
Davide <davide.n2@gmx.de>
2023-03-03 16:56:00 +0100

    D-58/Mobile CSS/

--- js/header_sidebar.js
+++ js/header_sidebar.js
@@ -35,7 +35,6 @@ function setFocus(id, selectedID) {
     if (id !== 'contactContainer') {
         setFocusSideSection(id, selectedID);
     }
-    setFocusBubbleContact(selectedID)
     setFocusContactContainer(id, selectedID);
     if (!isNaN(selectedID)) {
         previousID = selectedID;
@@ -46,16 +45,6 @@ function setFocus(id, selectedID) {
 function setFocusSideSection(id, selectedID) {
     document.getElementById(`${id}${selectedID}`).focus();
     document.getElementById(`${id}${selectedID}`).classList.add("focusSidebar");
-}
-
-
-function setFocusBubbleContact(selectedID) {
-    // try {
-        // document.getElementById(`contactBubble${selectedID}`).focus();
-        // document.getElementById(`contactBubble${selectedID}`).classList.add("contactsBubbleBorder");
-    // } catch (error) {
-    //     console.log("no Contact Bubble");
-    // }
 }
 
 
@@ -75,18 +64,18 @@ async function setFocusContactContainer(id, selectedID) {
 function openLogout() {
     let logoutContainer = document.getElementById('headerPopupDiv')
     logoutContainer.classList.toggle('d-none')
-  
+
     document.addEventListener('click', handleClickOutside)
-  }
-  
-  function handleClickOutside(e) {//Die Funktion handleClickOutside wird aufgerufen, wenn es einen Klick gibt.
+}
+
+function handleClickOutside(e) {//Die Funktion handleClickOutside wird aufgerufen, wenn es einen Klick gibt.
     let logoutContainer = document.getElementById('headerPopupDiv')//Der DOM-Element mit der ID headerPopupDiv wird in der Variable logoutContainer gespeichert.
     if (!e.target.closest('#headerPopupDiv') && !e.target.closest('#currentUserInitials')) {//Dann wird geprüft, ob das Element, auf das geklickt wurde, innerhalb des "headerPopupDiv"-Elements oder des "currentUserInitials"-Elements liegt. Wenn ja, wird nichts weiter gemacht.
-      logoutContainer.classList.add('d-none')//  Wenn das geklickte Element nicht innerhalb dieser beiden Elemente liegt, wird die CSS-Klasse "d-none" dem "logoutContainer"-Element hinzugefügt, um es auszublenden.
-      document.removeEventListener('click', handleClickOutside)//  Schließlich wird der Event-Listener für das "click"-Event entfernt, um zu verhindern, dass die Funktion erneut aufgerufen wird, wenn auf der Seite geklickt wird.
+        logoutContainer.classList.add('d-none')//  Wenn das geklickte Element nicht innerhalb dieser beiden Elemente liegt, wird die CSS-Klasse "d-none" dem "logoutContainer"-Element hinzugefügt, um es auszublenden.
+        document.removeEventListener('click', handleClickOutside)//  Schließlich wird der Event-Listener für das "click"-Event entfernt, um zu verhindern, dass die Funktion erneut aufgerufen wird, wenn auf der Seite geklickt wird.
     }
-  }
- 
+}
+
 
 function logout() {
     currentUser = ""
@@ -101,23 +90,23 @@ function getInitialForHeader() {
     currentUserInitials.innerHTML = `
       <div onclick="openLogout()" style="background:${color}">${initialLetters}</div>
     `;
-  }
+}
 
-  function goToEdit() {
+function goToEdit() {
     const name = currentUser.name;
     window.location.href = `contacts.html?edit=${name}`;
-  }
+}
 
-  function getQueryParam(param) {
+function getQueryParam(param) {
     const urlParams = new URLSearchParams(window.location.search);
     return urlParams.get(param);
-  }
-  
-  const nameToEdit = (getQueryParam('edit'));
-  
-  if (nameToEdit) {
+}
+
+const nameToEdit = (getQueryParam('edit'));
+
+if (nameToEdit) {
     setTimeout(() => {
-       openContact('contactContainer', 0); 
-       openEditContact(0)
+        openContact('contactContainer', 0);
+        openEditContact(0)
     }, 350);
-  }
+}
