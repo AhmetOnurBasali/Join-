a39308d647d95f2fe5c4094fa0650dceb015a37d
PhilippRandau <philipp-randau@web.de>
2023-03-15 22:27:20 +0100

    board jsdoc

--- js/board.js
+++ js/board.js
@@ -112,12 +112,16 @@ function progressSubtasks(task) {
 
 
 //---------------------------Drag and Drop Tasks---------------------------
+
 /**
- * This function is used to check if the dragged task could be drop in that area.
- * @param {event} ev - Event
- * @param {string} area - Identifies the area.
- * @param {string} areaID - Element ID of a area.
- */
+ * Allows dropping a dragged element onto an area and updates the current area on dragover.
+ * If the area is different from the current area of the dragged element and taskPreview is false,
+ * highlights the area by showing a task preview. If the area is the same and a task preview is shown,
+ * removes the task preview.
+ * @param {Event} ev - The drag event.
+ * @param {string} area - The area where the element is being dragged to.
+ * @param {string} areaID - The ID of the area element.
+*/
 function allowDrop(ev, area, areaID) {
     ev.preventDefault();
     currentAreaOndragover = area;
@@ -160,13 +164,19 @@ function startDragging(id) {
 }
 
 
-
+/**
+ * Checks the size of the current dragged task element and sets the minimum height of the task preview element to match it.
+ */
 function checkDraggedTaskSize() {
     let task = document.getElementById(`taskNumber_${currentDraggedElement}`);
     let actTaskHeight = task.offsetHeight;
     document.getElementById('task-preview').style.minHeight = actTaskHeight + 'px';
 }
 
+
+/**
+ * Moves the current dragged task element to the designated area and updates the board with the new order of tasks.
+ */
 async function moveTo() {
     allTasks[currentDraggedElement]['area'] = currentAreaOndragover;
     renderBoard();
@@ -174,17 +184,30 @@ async function moveTo() {
 }
 
 
+/**
+ * Sets the background color of the specified task element based on its "titleBg" property.
+ * @param {Object} task - The task object for which to set the background color.
+ * @param {string} eID - The ID of the element to set the background color on.
+ */
 function setTitleBg(task, eID) {
     document.getElementById(`${eID}${task['id']}`).style.backgroundColor = `${task['titleBg']}`;
 }
 
 
+/**
+ * Applies a CSS animation to a dragged task.
+ * @param {number} id - The id of the task being dragged.
+ */
 function dragAnimation(id) {
     document.getElementById(`taskNumber_${id}`).style.rotate = '4deg';
     document.getElementById(`taskNumber_${id}`).style.opacity = '0.75';
 }
 
 
+/**
+ * Adds a task to a board and displays a popup window for creating the task.
+ * @param {string} area - The name of the board, where the task will be added.
+ */
 function addTaskBoard(area) {
     isAddTask = true;
     document.getElementById("addTask").innerHTML = addTaskHTML();
@@ -200,16 +223,28 @@ function addTaskBoard(area) {
 }
 
 
+/**
+ * Disables scrolling on the body element to prevent the user from scrolling while a popup window is open.
+ */
 function preventScrolling() {
     return body.style.overflow = "hidden";
 }
 
 
+/**
+ * Sets the current board area where a new task will be added.
+ * @param {string} area - The name of the board area.
+ */
 function selectArea(area) {
     newArea = area;
 }
 
 
+/**
+ * Closes the Add Task Board by sliding it out of view and adding a delay for the animation to finish.
+ * Restores the navbar and the Create Task button for mobile view.
+ * Allows the user to scroll again.
+ */
 function closeAddTaskBoard() {
     slideOutAnimation('addTask', 'popup-add-task-board');
     setTimeout(() => {
@@ -223,16 +258,29 @@ function closeAddTaskBoard() {
     allowScrolling();
 }
 
+/**
+ * Allows the user to scroll again by setting the body's overflow property to "auto".
+ * @returns - body.style.overflow = "auto"
+ */
 function allowScrolling() {
-    return body.style.overflow = "auto"
+    return body.style.overflow = "auto";
 }
 
+
+/**
+ * Prevents the Add Task Board from closing when the user clicks inside it by stopping the propagation of the event.
+ * @param {Event} event - The click event that triggered the function.
+ */
 function doNotCloseAddTaskBoard(event) {
     event.stopPropagation();
 }
 
 
 //---------------------------Move to Area Mobile---------------------------
+/**
+ * Hides the "move to" button for the current task's current area on mobile, to avoid duplicating it
+ * @param {string} taskID - the ID of the current task being moved
+ */
 function selectAreaOnMobile(taskID) {
     let moveToButtonIDs = ['mobile-move-todo', 'mobile-move-inProgress', 'mobile-move-awaitingFeedback', 'mobile-move-done'];
     for (let i = 0; i < moveToButtonIDs.length; i++) {
@@ -244,6 +292,11 @@ function selectAreaOnMobile(taskID) {
 }
 
 
+/**
+ * Moves the specified task to the selected area on mobile, updates the board and saves to backend
+ * @param {string} taskID - the ID of the task being moved
+ * @param {string} selectedArea - the area the task is being moved to
+ */
 function moveToOnMobile(taskID, selectedArea) {
     allTasks[taskID]['area'] = selectedArea;
     renderBoard();
@@ -254,6 +307,10 @@ function moveToOnMobile(taskID, selectedArea) {
 
 
 //---------------------------Details Tasks---------------------------
+/**
+ * Displays the task details popup and renders the task details in it.
+ * @param {number} taskID - The ID of the task to display details for. 
+ */
 function openTaskDetailsFront(taskID) {
     currentTaskID = taskID;
     document.getElementById('popup-task-details').classList.remove('d-none');
@@ -267,6 +324,11 @@ function openTaskDetailsFront(taskID) {
 }
 
 
+/**
+ * Renders the subtasks of a given task in the specified HTML element.
+ * @param {object} task - The task object containing the subtasks to render.
+ * @param {string} eID - The ID of the HTML element to render the subtasks in.
+ */
 function renderSubtasks(task, eID) {
     document.getElementById(`${eID}${task['id']}`).innerHTML = '';
 
@@ -285,6 +347,11 @@ function renderSubtasks(task, eID) {
 }
 
 
+/**
+ * This asynchronous function updates the status of a subtask and re-renders the subtask list and board.
+ * @param {number} i - Index of the subtask to be updated.
+ * @param {string} subtaskID - The ID of the subtask element in the HTML.
+ */
 async function statusSubtask(i, subtaskID) {
     let statusSubtask = document.getElementById(subtaskID);
     if (statusSubtask.checked) {
@@ -301,11 +368,18 @@ async function statusSubtask(i, subtaskID) {
 }
 
 
+/**
+ * This function sets the background color of the task priority sign based on the priority level of the current task.
+ */
 function setPriorityBg() {
     document.getElementById(`task-details-prio-sign${currentTaskID}`).style.backgroundColor = `${setPriorityBgColor()}`;
 }
 
 
+/**
+ * This function returns the background color for the priority sign based on the priority level of the current task.
+ * @returns {string} The background color for the priority sign as a string.
+ */
 function setPriorityBgColor() {
     switch (allTasks[currentTaskID]['prio']) {
         case 'low':
@@ -319,7 +393,9 @@ function setPriorityBgColor() {
     }
 }
 
-
+/**
+ * Closes the task details popup and restores the body's overflow to 'auto'.
+ */
 function closeTaskDetails() {
     document.getElementById('body').style.overflow = 'auto';
     slideAssignTo = false;
@@ -330,7 +406,9 @@ function closeTaskDetails() {
     }, animationTimeout);
 }
 
-
+/**
+ * Renders the task details popup with an editable form to edit the task's details.
+ */
 function editDetailsTask() {
     document.getElementById('task-details').innerHTML = renderEditDetailsTaskHTML();
     renderSelectContactEdit();
@@ -338,6 +416,10 @@ function editDetailsTask() {
     setPrioCheckBox(allTasks[currentTaskID]['prio'], 'Edit');
 }
 
+
+/**
+ * Renders the HTML for the select contact dropdown menu in the edit task details popup.
+ */
 function renderSelectContactEdit() {
     let contactInitials = document.getElementById('contactInitialsEdit');
     contactInitials.innerHTML = ``;
@@ -349,6 +431,10 @@ function renderSelectContactEdit() {
 }
 
 
+/**
+ * Checks the value of openSubtask and closedSubtask arrays of a task and removes the task subtasks section from the DOM if the sum of both arrays is 0.
+ * @param {object} task - The task object to check its subtasks value.
+ */
 function checkValueOfSubtasks(task) {
     if (task['openSubtask'].length + task['closedSubtask'].length == 0) {
         document.getElementById(`task-subtasks${task['id']}`).remove();
@@ -356,8 +442,11 @@ function checkValueOfSubtasks(task) {
 }
 
 
-async function editExistingTask(event) {
-    
+/**
+ * Edits an existing task with the data entered in the edit task form and updates it in the allTasks array.
+ * @param {Event} event - The event object that triggered the function.
+ */
+function editExistingTask(event) {
     proofEvent(event);
     let editTask = getTaskDataEdit();
     let proof = taskProofSectionEdit(editTask);
@@ -368,6 +457,10 @@ async function editExistingTask(event) {
 }
 
 
+/**
+ * Gets the data entered in the edit task form and returns an object with the values.
+ * @returns {object} - An object with the values entered in the edit task form.
+ */
 function getTaskDataEdit() {
     let titleEdit = document.getElementById("titleEdit").value;
     let descriptionEdit = document.getElementById("descriptionEdit").value;
@@ -383,6 +476,11 @@ function getTaskDataEdit() {
 }
 
 
+/**
+ * Checks if the input values entered in the edit task form are valid, and returns true if they are.
+ * @param {object} editTask - An object with the edited task values entered in the form.
+ * @returns {boolean} - true if all input values are valid, false otherwise.
+ */
 function taskProofSectionEdit(editTask) {
     let title = proofTitle(editTask, 'Edit');
     let description = proofDescription(editTask, 'Edit');
@@ -391,13 +489,21 @@ function taskProofSectionEdit(editTask) {
     let prio = proofPrio(editTask, 'Edit');
     if (checkProofOfEdit(title, description, assigned, date, prio) === true) {
         return true;
-    }else{
+    } else {
         return false;
     }
-    
 }
 
 
+/**
+ * Checks if all proof conditions are met and returns true if they are.
+ * @param {boolean} title - The title proof result.
+ * @param {boolean} description - The description proof result.
+ * @param {boolean} assigned - The assigned proof result.
+ * @param {boolean} date - The date proof result.
+ * @param {boolean} prio - The priority proof result.
+ * @returns {boolean} - true if all proof conditions are met, false otherwise.
+ */
 function checkProofOfEdit(title, description, assigned, date, prio) {
     return (
         title === true &&
@@ -409,6 +515,10 @@ function checkProofOfEdit(title, description, assigned, date, prio) {
 }
 
 
+/**
+ * Edits the task data in the allTasks array with the data entered in the edit task form, saves it in the backend, and re-renders the board.
+ * @param {object} editTask - An object with the edited task values entered in the form.
+ */
 async function editTaskData(editTask) {
     allTasks[currentTaskID]['title'] = editTask['title'];
     allTasks[currentTaskID]['description'] = editTask['description'];
